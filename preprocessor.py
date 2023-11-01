edd8548b3861c8156ed6a2e01273e362e4472937
soujanyacheruvu <127056561+soujanyacheruvu@users.noreply.github.com>
2023-11-01 13:00:34 -0400

    ..

--- preprocessor.py
+++ preprocessor.py
@@ -7,45 +7,36 @@ def preprocess(data):
     messages = re.split(pattern, data)[1:]
     dates = re.findall(pattern, data)
 
-    df = pd.DataFrame({'user_message': messages, 'message_date': dates})
-    # convert message_date type
+    if len(messages) != len(dates):
+        raise ValueError("Mismatch in the number of dates and messages.")
+
+    df = pd.DataFrame({'message_date': dates, 'user_message': messages})
     df['message_date'] = pd.to_datetime(df['message_date'], format='%d/%m/%Y, %H:%M - ')
 
-    df.rename(columns={'message_date': 'date'}, inplace=True)
+    df['user'] = df['user_message'].str.extract('([^:]+):', expand=False).fillna('group_notification')
+    df['message'] = df['user_message'].str.split(': ').str[1]
 
-    users = []
-    messages = []
-    for message in df['user_message']:
-        entry = re.split('([\w\W]+?):\s', message)
-        if entry[1:]:  # user name
-            users.append(entry[1])
-            messages.append(" ".join(entry[2:]))
-        else:
-            users.append('group_notification')
-            messages.append(entry[0])
-
-    df['user'] = users
-    df['message'] = messages
     df.drop(columns=['user_message'], inplace=True)
 
-    df['only_date'] = df['date'].dt.date
-    df['year'] = df['date'].dt.year
-    df['month_num'] = df['date'].dt.month
-    df['month'] = df['date'].dt.month_name()
-    df['day'] = df['date'].dt.day
-    df['day_name'] = df['date'].dt.day_name()
-    df['hour'] = df['date'].dt.hour
-    df['minute'] = df['date'].dt.minute
+    df['only_date'] = df['message_date'].dt.date
+    df['year'] = df['message_date'].dt.year
+    df['month_num'] = df['message_date'].dt.month
+    df['month'] = df['message_date'].dt.strftime('%B')
+    df['day'] = df['message_date'].dt.day
+    df['day_name'] = df['message_date'].dt.strftime('%A')
+    df['hour'] = df['message_date'].dt.hour
+    df['minute'] = df['message_date'].dt.minute
 
-    period = []
-    for hour in df[['day_name', 'hour']]['hour']:
-        if hour == 23:
-            period.append(str(hour) + "-" + str('00'))
-        elif hour == 0:
-            period.append(str('00') + "-" + str(hour + 1))
-        else:
-            period.append(str(hour) + "-" + str(hour + 1))
-
-    df['period'] = period
+    df['period'] = df['message_date'].apply(lambda x: f"{x.hour:02d}-{(x.hour + 1) % 24:02d}")
 
     return df
+
+# Example usage:
+data = """
+10/01/2023, 14:30 - User1: Hello!
+10/01/2023, 14:35 - User2: Hi there!
+10/01/2023, 14:40 - User1: How are you?
+"""
+
+df = preprocess(data)
+print(df)
